7e7fa177f51cf58b0c2ba0df0dd5270a33c4b791
tiagohasse <tiagoh_n@hotmail.com>
2024-10-19 19:01:26 -0300

    Refactor: Adjust footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,8 +6,8 @@ export default function Footer() {
       <div className="container mx-auto px-4 py-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           <div>
-            <h3 className="text-lg font-semibold mb-4">Sobre Última Chance</h3>
-            <p>Economize hoje, preserve amanhã.</p>
+            {/* <h3 className="text-lg font-semibold mb-4">Sobre Última Chance</h3> */}
+            <p className="font-semibold">Economize hoje, preserve amanhã.</p>
           </div>
           <div>
             {/* <h3 className="text-lg font-semibold mb-4">Links</h3> */}
