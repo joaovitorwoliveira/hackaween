e669006d84c6811602f02fa8216731c30032004f
Joao Vitor Oliveira <joaovitorwoliveira@gmail.com>
2024-10-19 16:42:46 -0300

    add: add and remove funcionality at cart

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from "react";
-
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
@@ -35,15 +34,38 @@ export default function CartPage() {
     }
   }, []);
 
-  const updateQuantity = (id: number, newQuantity: number) => {
-    const updatedCart = cart
-      .map((item) =>
-        item.id === id ? { ...item, quantity: Math.max(0, newQuantity) } : item
-      )
-      .filter((item) => item.quantity > 0);
-
+  const saveCartToLocalStorage = (
+    updatedCart: { id: number; quantity: number }[]
+  ) => {
     setCart(updatedCart);
     localStorage.setItem("cartItems", JSON.stringify(updatedCart));
+  };
+
+  const addToCart = (id: number) => {
+    const updatedCart = cart.map((item) =>
+      item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+    );
+    saveCartToLocalStorage(updatedCart);
+  };
+
+  const removeFromCart = (id: number) => {
+    const updatedCart = cart
+      .map((item) =>
+        item.id === id ? { ...item, quantity: item.quantity - 1 } : item
+      )
+      .filter((item) => item.quantity > 0); // Remove o item se a quantidade for 0
+    saveCartToLocalStorage(updatedCart);
+  };
+
+  const updateQuantity = (id: number, newQuantity: number) => {
+    if (newQuantity <= 0) {
+      removeFromCart(id);
+    } else {
+      const updatedCart = cart.map((item) =>
+        item.id === id ? { ...item, quantity: newQuantity } : item
+      );
+      saveCartToLocalStorage(updatedCart);
+    }
   };
 
   const cartProducts = cart
@@ -55,7 +77,7 @@ export default function CartPage() {
 
   const total = cartProducts.reduce((sum, item) => {
     if (!item) return sum;
-    return sum + item.price * (item.quantity || 1);
+    return sum + item.discountPrice * (item.quantity || 1);
   }, 0);
 
   return (
@@ -68,10 +90,11 @@ export default function CartPage() {
         <Table>
           <TableHeader>
             <TableRow>
-              <TableHead>Product</TableHead>
-              <TableHead>Price</TableHead>
-              <TableHead>Quantity</TableHead>
+              <TableHead>Produto</TableHead>
+              <TableHead>Preço</TableHead>
+              <TableHead>Quantidade</TableHead>
               <TableHead>Total</TableHead>
+              <TableHead>Ações</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
@@ -81,7 +104,12 @@ export default function CartPage() {
               return (
                 <TableRow key={item.id}>
                   <TableCell>{item.name}</TableCell>
-                  <TableCell>${item.discountPrice.toFixed(2)}</TableCell>
+                  <TableCell>
+                    {item.discountPrice.toLocaleString("pt-br", {
+                      style: "currency",
+                      currency: "BRL",
+                    })}
+                  </TableCell>
                   <TableCell>
                     <Input
                       type="number"
@@ -89,12 +117,32 @@ export default function CartPage() {
                       onChange={(e) =>
                         updateQuantity(item.id, parseInt(e.target.value))
                       }
-                      min="0"
+                      min="1"
                       className="w-20"
                     />
                   </TableCell>
                   <TableCell>
-                    ${(item.discountPrice * (item.quantity || 1)).toFixed(2)}
+                    {(item.discountPrice * (item.quantity || 1)).toLocaleString(
+                      "pt-br",
+                      {
+                        style: "currency",
+                        currency: "BRL",
+                      }
+                    )}
+                  </TableCell>
+                  <TableCell className="flex gap-2">
+                    <Button
+                      className="bg-gray-400"
+                      onClick={() => removeFromCart(item.id)}
+                    >
+                      -
+                    </Button>
+                    <Button
+                      className="bg-gray-400"
+                      onClick={() => addToCart(item.id)}
+                    >
+                      +
+                    </Button>
                   </TableCell>
                 </TableRow>
               );
@@ -102,8 +150,13 @@ export default function CartPage() {
           </TableBody>
         </Table>
         <div className="mt-8 text-right">
-          <p className="text-xl font-bold">Total: ${total.toFixed(2)}</p>
-
+          <p className="text-xl font-bold">
+            Total:{" "}
+            {total.toLocaleString("pt-br", {
+              style: "currency",
+              currency: "BRL",
+            })}
+          </p>
           <Dialog>
             <DialogTrigger asChild>
               <Button className="mt-4">Seguir com a entrega</Button>
